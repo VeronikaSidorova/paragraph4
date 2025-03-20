9cd37d19e233df36fd53851a368991a558432ece
Veronika Sidorova <veronikamartyn2106@yandex.ru>
2025-03-20 20:25:58 +0300

    task1

--- src/product.py
+++ src/product.py
@@ -6,3 +6,7 @@ class Product:
     quantity: int
 
     def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
